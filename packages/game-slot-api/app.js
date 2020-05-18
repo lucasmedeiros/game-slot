a220d034aae5f7f49a02535ceb17ddd782f940b2
Lucas Medeiros <lucasmed812@gmail.com>
2020-05-18 19:52:06 -0300

    Add home route

--- packages/game-slot-api/app.js
+++ packages/game-slot-api/app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ ok: true })
 })
 app.use('/steam', steam)
