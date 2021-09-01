fd6ce441f273cc634ba93c405fcde565902c3df9
Jadson Luan <jadson.silva@splab.ufcg.edu.br>
2021-09-01 10:40:41 -0300

    Change login route method to GET

--- game-slot/backend/routes/auth.js
+++ game-slot/backend/routes/auth.js
@@ -4,7 +4,7 @@ const { checkUser } = require('../middlewares/validateUser')
 const { signup, login, remove } = require('../controllers/auth')
 const router = express.Router()
 
-router.post('/', checkJwt, login)
+router.get('/', checkJwt, login)
 router.post('/signup', checkJwt, signup)
 router.delete('/:id', checkJwt, checkUser, remove)
 
