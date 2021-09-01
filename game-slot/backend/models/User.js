f330ad76d81bc92e09f3b47611d6751fb50fb3eb
Jadson Luan <jadson.silva@splab.ufcg.edu.br>
2021-09-01 10:34:48 -0300

    Remove user password field

--- game-slot/backend/models/User.js
+++ game-slot/backend/models/User.js
@@ -9,10 +9,6 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
 })
 
 module.exports = model('User', UserSchema)
