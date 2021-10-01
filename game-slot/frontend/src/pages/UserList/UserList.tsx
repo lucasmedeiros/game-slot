594db906f69ea5eeb3087a69da41e9d6676dbdbc
Lucas Medeiros <lucasmed812@gmail.com>
2021-10-01 19:10:45 -0300

    Remove postcss from build

--- game-slot/frontend/src/pages/UserList/UserList.tsx
+++ game-slot/frontend/src/pages/UserList/UserList.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { ClipLoader } from 'react-spinners'
-import { Link } from 'react-router-dom'
 
 import { useCurrentUser } from '../../contexts/UserContext'
 import UserListsDisplay from './UserListsDisplay'
